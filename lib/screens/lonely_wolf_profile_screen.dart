518f9602026813c66e5b495523fdf08e7a431514
Giovane <giovanelopes@id.uff.br>
2019-11-24 17:56:49 -0300

    mapa funcionando, mas com warnings

--- lib/screens/lonely_wolf_profile_screen.dart
+++ lib/screens/lonely_wolf_profile_screen.dart
@@ -1,14 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:projsoftware/components/UI/drawer.dart';
 import 'package:projsoftware/components/UI/profiles.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LonelyWolf extends StatelessWidget {
+
+  // Completer<GoogleMapController> _controller = Completer();
+  // static const LatLng _center =
+  //     const LatLng(-22.8808,  -43.1043);
+  // final Set<Marker> _markers = {};
+  // LatLng _lastMapPosition = _center;
+  // MapType _currentMapType = MapType.normal;
+
+  // _onMapCreated(GoogleMapController controller) {
+  //   _controller.complete(controller);
+  // }
+
+  // _onCameraMove(CameraPosition position) {
+  //   _lastMapPosition = position.target;
+  // }
+
+  // Widget button(Function function, IconData icon) {
+  //   return FloatingActionButton(
+  //     onPressed: function,
+  //     materialTapTargetSize: MaterialTapTargetSize.padded,
+  //     backgroundColor: Colors.blue,
+  //     child: Icon(icon, size: 36.0),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return _buildLonenlyWolfScreen();
+    return _buildLonenlyWolfScreen(context);
   }
 
-  Widget _buildLonenlyWolfScreen() {
+  Widget _buildLonenlyWolfScreen(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(),
       drawer: new AppDrawer(),
@@ -19,6 +48,16 @@ class LonelyWolf extends StatelessWidget {
             onTap: () {
               debugPrint("tempo ruim");
             },
+          ),
+          SizedBox(
+            width: width,
+            height: 500,
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+              target: LatLng(-22.8808,  -43.1043),
+              zoom: 12.0
+              ),
+            ),
           ),
         ],
       ),
--- lib/screens/outgoing_screen.dart
+++ lib/screens/outgoing_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:projsoftware/components/UI/drawer.dart';
 import 'package:projsoftware/components/UI/profiles.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Outgoing extends StatelessWidget {
   @override
